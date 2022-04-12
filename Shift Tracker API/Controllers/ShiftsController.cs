f946d092db104c28ce9dcaa217022de458306c66
Velrosa <steezie21@hotmail.co.uk>
2022-04-12 15:48:27 +0100

    Implemented ShiftsController CRUD functionality. Tested HTTP requests with Postman.

---
+++ Shift Tracker API/Controllers/ShiftsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shift_Tracker_API.Models;
+using Shift_Tracker_API.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shift_Tracker_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShiftsController : ControllerBase
+    {
+        private readonly IShiftRepository _shiftrepository;
+
+        public ShiftsController(IShiftRepository shiftRepository)
+        {
+            _shiftrepository = shiftRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Shift>> GetShifts()
+        {
+            return await _shiftrepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Shift>> GetShifts(int id)
+        {
+            return await _shiftrepository.Get(id);
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<Shift>> PostShifts([FromBody] Shift shift)
+        {
+            var newShift = await _shiftrepository.Create(shift);
+            return CreatedAtAction(nameof(GetShifts), new { id = newShift.ShiftId }, newShift);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutShifts(int id, [FromBody] Shift shift)
+        {
+            if(id != shift.ShiftId)
+            {
+                return BadRequest();
+            }
+
+            await _shiftrepository.Update(shift);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var shiftToDelete = await _shiftrepository.Get(id);
+            if(shiftToDelete == null) { return NotFound(); }
+
+            await _shiftrepository.Delete(shiftToDelete.ShiftId);
+            return NoContent();
+        }
+    }
+}
